58ec437629be3d2042189930acb8c6bcc206b994
Serrière Guillaume <guillaume.serriere@telecomnancy.net>
2017-02-18 23:05:27 +0100

    All: Add main and start of RESTInterface.
    Signed-off-by: Serrière Guillaume <guillaume.serriere@telecomnancy.net>

---
+++ src/main/java/core/Main.java
@@ -0,0 +1,34 @@
+package core;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+    public static final String BASE_URI = "http://localhost:8080/myapp/";
+
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("core");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.in.read();
+        server.shutdown();
+    }
+}
+
